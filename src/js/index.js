c00599de6c85d6912dbeeb6adc07edb7a1cc3796
José Teles <josetelesmaciel@gmail.com>
2023-03-25 18:33:50 -0300

    feat(#3): Restores project structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs'
 import pomodoro from './pomodoro'
+// import persist from '@alpinejs/persist'
 import '../css/index.css'
-Alpine.data('pomodoro', pomodoro)
 
+Alpine.data('pomodoro', pomodoro)
+// Alpine.plugin(persist)
 Alpine.start()
