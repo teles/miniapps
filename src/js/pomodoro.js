e638e42c70abd7a175542a0d178785885fb81e46
José Teles <josetelesmaciel@gmail.com>
2023-03-26 19:44:11 -0300

    refactor(no-issue): applies small refactorings

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -3,6 +3,16 @@ import persist from '@alpinejs/persist'
 import '../css/index.css'
 
 const pomodoro = function () {
+    const seconds_pomodoro = 25 * 60
+    const get_mockup_pomodoro = (text, state) => ({
+        text,
+        state,
+        is_editing: false,
+        time_left: seconds_pomodoro,
+        started_at: new Date,
+        finished_at: null
+    })
+
     return {
         icons: {
             cog: 'M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z',
@@ -36,35 +46,30 @@ const pomodoro = function () {
                 }
             ]
         }),
+        states: {
+            running: 'running',
+            paused: 'paused',
+            finished: 'finished',
+            breaking: 'breaking',
+            archived: 'archived'
+        },
         pomodoros: Alpine.$persist([
-            {
-                text: 'Buy tomatos',
-                state: 'running',
-                is_editing: false,
-                countdown: 60 * 25,
-                started_at: new Date,
-                finished_at: null
-            },
-            {
-                text: 'Defeat Thanos and save the entire universe',
-                state: 'paused',
-                is_editing: false,
-                countdown: 60 * 25,
-                started_at: new Date,
-                finished_at: null
-            }
+            get_mockup_pomodoro('Buy tomatos', 'running'),
+            get_mockup_pomodoro('Defeat Thanos and save the entire universe', 'paused'),
+            get_mockup_pomodoro('Wash dishes', 'finished')
         ]),
         get archived() {
-            return this.pomodoros.filter(pomodoro => pomodoro.state === 'archived')
+            return this.pomodoros.filter(pomodoro => pomodoro.state === this.states.archived)
         },
         get has_pomorodo_running() {
-            return this.pomodoros.filter(pomodoro => pomodoro.state === 'running').length === 1
+            return this.pomodoros.filter(pomodoro => pomodoro.state === this.states.running).length === 1
         },
         countdown(pomodoro) {
-            if (pomodoro.countdown > 0 && pomodoro.state === 'running') {
-                pomodoro.countdown = pomodoro.countdown - 1;
-            } else if (pomodoro.countdown === 0 && pomodoro.state === 'running') {
-                pomodoro.state = 'finished';
+            if (pomodoro.time_left > 0 && pomodoro.state === this.states.running) {
+                pomodoro.time_left = pomodoro.time_left - 1
+            } else if (pomodoro.time_left === 0 && pomodoro.state === this.states.running) {
+                pomodoro.state = this.states.finished
+                pomodoro.time_left = 0
                 pomodoro.finished_at = new Date()
             }
         },
@@ -74,8 +79,8 @@ const pomodoro = function () {
             if (this.new_pomodoro_text.length > 0) {
                 this.pomodoros.push({
                     text: this.new_pomodoro_text,
-                    state: 'paused',
-                    countdown: 25 * 60,
+                    state: this.states.paused,
+                    time_left: 25 * 60,
                     is_editing: false,
                     started_at: new Date,
                     finished_at: null
@@ -83,8 +88,8 @@ const pomodoro = function () {
             }
             this.new_pomodoro_text = '';
         },
-        save(pomodoro) {
-            pomodoro.state = 'archived'
+        archive(pomodoro) {
+            pomodoro.state = this.states.archived
         },
         run(pomodoro) {
             this.pomodoros = this.pomodoros.map(item => {
@@ -99,13 +104,14 @@ const pomodoro = function () {
             });
         },
         remove(pomodoro) {
-            if (pomodoro.state !== 'running') {
+            if (pomodoro.state !== this.states.running) {
                 this.pomodoros = this.pomodoros.filter(pomodoro_item => pomodoro_item !== pomodoro)
             }
         }
     }
 }
 
+window.pomodoro = pomodoro
 Alpine.data('pomodoro', pomodoro)
 Alpine.plugin(persist)
 Alpine.start()
