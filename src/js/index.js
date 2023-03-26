b55829dc9acb3c614cbcdfadcaca189e158f2d20
José Teles <josetelesmaciel@gmail.com>
2023-03-25 21:32:31 -0300

    Revert "feat(#3): Adds persist plugin to project"
    This reverts commit ed01cafbf130325e85e3e02758d9c65fc2381148.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-import pomodoro from './pomodoro'
-import persist from '@alpinejs/persist'
 import Alpine from 'alpinejs'
+import pomodoro from './pomodoro'
+// import persist from '@alpinejs/persist'
 import '../css/index.css'
 
-Alpine.plugin(persist)
 Alpine.data('pomodoro', pomodoro)
+// Alpine.plugin(persist)
 Alpine.start()
